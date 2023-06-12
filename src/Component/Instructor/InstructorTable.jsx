f601c3c8c75540dc710af9ed9245ddefc64641a1
Abu Bokor <abubokor1066@gmail.com>
2023-06-12 10:18:51 +0600

    instructor page implement

--- src/Component/Instructor/InstructorTable.jsx
+++ src/Component/Instructor/InstructorTable.jsx
@@ -4,13 +4,13 @@ const InstructorTable = ({ instructor, index }) => {
   console.log(instructor);
   const { email, image, Name, _id } = instructor;
   return (
-    <tr>
+    <tr className="bg-gradient-to-r border-y-2 border-black from-purple-500 to-pink-500">
       <th>{index}</th>
       <td>
         <img src={image} className="w-20 rounded-3xl" alt="" />
       </td>
-      <td>{Name}</td>
-      <td>{email}</td>
+      <td className="font-semibold text-white">{Name}</td>
+      <td className="font-semibold text-white">{email}</td>
 
       <th>
         <button className="btn btn-success">See Classes</button>
